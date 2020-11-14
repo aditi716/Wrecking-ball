e00188ab5d3db7c17fcc4916cef87780f95e8532
aditi716 <adidiv309@gmail.com>
2020-11-14 12:49:37 +0530

    wrecking-ball

---
+++ Rope.js
@@ -0,0 +1,20 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options ={
+            bodyA : bodyA,
+            pointB : pointB,
+            stiffness : 1.2,
+            length : 250
+        }
+        this.pointB = pointB;
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+    display(){
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke("purple");
+        line(pointB.x, pointB.y, pointA.x, pointA.y);
+    }
+}
